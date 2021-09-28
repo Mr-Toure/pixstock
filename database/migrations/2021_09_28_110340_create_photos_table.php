c24226af769fe5ab34c5a620e21af1f5467aac6e
Toure Simplice <toure-simplice@outlook.fr>
2021-09-28 11:28:49 +0000

    creat model source -mf

---
+++ database/migrations/2021_09_28_110340_create_photos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('album_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->string('slug')->nullable();
+            $table->string('thumbnail_path')->nullable();
+            $table->string('thumbnail_url')->nullable();
+            $table->boolean('active')->nullable()->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photos');
+    }
+}
